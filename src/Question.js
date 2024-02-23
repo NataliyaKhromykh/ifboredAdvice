67a2f690859fbd6e6e045fd9a53b629a2c434561
Nataliya Khromykh <nataliyakhromykh@Nataliyas-MBP.attlocal.net>
2024-02-22 23:18:29 -0500

    first commit

---
+++ src/Question.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import picture from './picture.png'
+
+function Question(){
+
+    const [advice, setAdvice] = useState('');
+
+
+    // useEffect(() =>{
+    //     getAdvice()
+    // },[])
+    const getCurrentDate = () => {
+        const currentDate = new Date();
+        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        return currentDate.toLocaleDateString('en-US', options);
+    };
+
+    const getAdvice = async() =>{
+        const response = await fetch(`http://www.boredapi.com/api/activity`);
+        const data = await response.json();
+        setAdvice(data.activity);
+    }
+
+    return(
+        <div className='mainDiv'>
+            <img className='backgroundPic' src={picture} alt='backgroundpic' />
+            <div className='box'>
+            <p className='date'>{getCurrentDate()}</p>
+        <button onClick={getAdvice} className='btnSearch'>
+        Maybe...
+        </button>
+        <p className='text'>{advice}</p>
+
+
+    </div>
+
+        </div>
+    )
+}
+export default Question;
